157e3ec4f1ace8e11a94a33e1b6a15985d08ee4a
jjuve <75339345+jjuve@users.noreply.github.com>
2024-07-29 15:31:07 -0500

    use selenim to log in and bs4 to parse html

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ from time import sleep
 from bs4 import BeautifulSoup
 import requests
 
+driver = webdriver.Chrome()
+
+sleep(5)
+
+driver.get("https://app.wodify.com/")
+
+sleep(5)
+
+driver.quit()
+
 def scrape():
-      url = 'https://www.walmart.com/'
+      url = 'https://app.wodify.com/'
       response = requests.get(url)
       soup = BeautifulSoup(response.text, 'html.parser')
       print(soup.prettify())
